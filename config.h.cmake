47bbf530330266ad0c0813293068258175838eae
Daniel Poelzleithner <git@poelzi.org>
2010-12-23 23:00:37 +0100

    add compilation fix.
    this is total ugly and needs detection with cmake

--- config.h.cmake
+++ config.h.cmake
@@ -6,4 +6,51 @@
 #cmakedefine ENABLE_NLS
 
 // FIXME need detection
-#define HAVE_STRUCT_UTMPX_UT_USER
+/* Define to 1 if `ut_exit' is a member of `struct utmpx'. */
+#define HAVE_STRUCT_UTMPX_UT_EXIT 1
+/* Define to 1 if `ut_exit.e_exit' is a member of `struct utmpx'. */
+#define HAVE_STRUCT_UTMPX_UT_EXIT_E_EXIT 1
+/* Define to 1 if `ut_exit.e_termination' is a member of `struct utmpx'. */
+#define HAVE_STRUCT_UTMPX_UT_EXIT_E_TERMINATION 1
+/* Define to 1 if `ut_exit.ut_exit' is a member of `struct utmpx'. */
+/* #undef HAVE_STRUCT_UTMPX_UT_EXIT_UT_EXIT */
+/* Define to 1 if `ut_exit.ut_termination' is a member of `struct utmpx'. */
+/* #undef HAVE_STRUCT_UTMPX_UT_EXIT_UT_TERMINATION */
+/* Define to 1 if `ut_id' is a member of `struct utmpx'. */
+#define HAVE_STRUCT_UTMPX_UT_ID 1
+/* Define to 1 if `ut_name' is a member of `struct utmpx'. */
+#define HAVE_STRUCT_UTMPX_UT_NAME 1
+/* Define to 1 if `ut_pid' is a member of `struct utmpx'. */
+#define HAVE_STRUCT_UTMPX_UT_PID 1
+/* Define to 1 if `ut_type' is a member of `struct utmpx'. */
+#define HAVE_STRUCT_UTMPX_UT_TYPE 1
+/* Define to 1 if `ut_user' is a member of `struct utmpx'. */
+#define HAVE_STRUCT_UTMPX_UT_USER 1
+/* Define to 1 if `ut_exit' is a member of `struct utmp'. */
+#define HAVE_STRUCT_UTMP_UT_EXIT 1
+/* Define to 1 if `ut_exit.e_exit' is a member of `struct utmp'. */
+#define HAVE_STRUCT_UTMP_UT_EXIT_E_EXIT 1
+/* Define to 1 if `ut_exit.e_termination' is a member of `struct utmp'. */
+#define HAVE_STRUCT_UTMP_UT_EXIT_E_TERMINATION 1
+/* Define to 1 if `ut_exit.ut_exit' is a member of `struct utmp'. */
+/* #undef HAVE_STRUCT_UTMP_UT_EXIT_UT_EXIT */
+/* Define to 1 if `ut_exit.ut_termination' is a member of `struct utmp'. */
+/* #undef HAVE_STRUCT_UTMP_UT_EXIT_UT_TERMINATION */
+/* Define to 1 if `ut_id' is a member of `struct utmp'. */
+#define HAVE_STRUCT_UTMP_UT_ID 1
+/* Define to 1 if `ut_name' is a member of `struct utmp'. */
+#define HAVE_STRUCT_UTMP_UT_NAME 1
+/* Define to 1 if `ut_pid' is a member of `struct utmp'. */
+#define HAVE_STRUCT_UTMP_UT_PID 1
+/* Define to 1 if `ut_type' is a member of `struct utmp'. */
+#define HAVE_STRUCT_UTMP_UT_TYPE 1
+/* Define to 1 if `ut_user' is a member of `struct utmp'. */
+#define HAVE_STRUCT_UTMP_UT_USER 1
+/* Define to 1 if you have the `utmpname' function. */
+#define HAVE_UTMPNAME 1
+/* Define to 1 if you have the `utmpxname' function. */
+#define HAVE_UTMPXNAME 1
+/* Define to 1 if you have the <utmpx.h> header file. */
+#define HAVE_UTMPX_H 1
+/* Define to 1 if you have the <utmp.h> header file. */
+#define HAVE_UTMP_H 1
