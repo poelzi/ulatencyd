06fee4e247e6d9e05cdbb164d8a01b4b3e5d8e66
Daniel Poelzleithner <git@poelzi.org>
2011-01-20 11:40:24 +0100

    release 0.3.5

--- src/ulatency.h
+++ src/ulatency.h
@@ -38,7 +38,7 @@
                                G_LOG_LEVEL_TRACE,    \
                                __VA_ARGS__)
 
-#define VERSION 0.3.1
+#define VERSION 0.3.5
 
 #define OPENPROC_FLAGS PROC_FILLMEM | \
   PROC_FILLUSR | PROC_FILLGRP | PROC_FILLSTATUS | PROC_FILLSTAT | \
