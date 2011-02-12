9dd55613a5eee6e589d7d161487f626281217fac
Daniel Poelzleithner <git@poelzi.org>
2011-02-13 00:16:41 +0100

    add jackd rule

--- rules/media.lua
+++ rules/media.lua
@@ -18,4 +18,18 @@ MediaPlayer = {
   end,
 }
 
+
+Jackd = {
+  name = "Jackd",
+  re_cmdline = "jackd",
+  --re_basename = "metacity",
+  check = function(self, proc)
+    local flag = ulatency.new_flag("user.bg_high")
+    proc:add_flag(flag)
+    rv = ulatency.filter_rv(ulatency.FILTER_STOP)
+    return rv
+  end,
+}
+
+ulatency.register_filter(Jackd)
 ulatency.register_filter(MediaPlayer)
