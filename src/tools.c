f81bfecd21351505bd94621c621ff82fac4e0dbb
Daniel Poelzleithner <poelzi@poelzi.org>
2011-02-17 19:44:19 +0100

    add missing file

---
+++ src/tools.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <fts.h>
+#include <unistd.h>
+
+
+void recursive_rmdir(const char *path, int add_level) {
+    FTS *fts;
+    FTSENT *ftsent;
+    char *const paths[] = { (char *)path, NULL };
+
+    /*
+     * This means there can't be any autofs mounts yet, so
+     * this is the first time we're being run since a reboot.
+     * Clean out any stuff left in /Network from the reboot.
+     */
+    fts = fts_open(paths, FTS_NOCHDIR|FTS_PHYSICAL,
+        NULL);
+    if (fts != NULL) {
+      while ((ftsent = fts_read(fts)) != NULL) {
+        /*
+         * We only remove directories - if
+         * there are files, we assume they're
+         * there for a purpose.
+         *
+         * We remove directories after we've
+         * removed their children, so we want
+         * to process directories visited in
+         * post-order.
+         */
+        if (ftsent->fts_info == FTS_DP &&
+            ftsent->fts_level >= FTS_ROOTLEVEL + add_level)
+          rmdir(ftsent->fts_accpath);
+      }
+      fts_close(fts);
+    }
+}
