1d220ab9f774469c834ce31401c61f8ce457b4fb
Daniel Poelzleithner <poelzi@poelzi.org>
2011-02-20 11:35:14 +0100

    add io scheduler rule for media players
    give media players a higher io priority

---
+++ rules/zz_fixers.lua
@@ -0,0 +1,17 @@
+-- last rules to execute, they can depend on flags set by previous rules
+
+MediaIO = {
+  name = "MediaIO",
+  check = function(self, proc)
+    -- we give processes marked with media flags good io prio
+    if ulatency.find_flag(proc:list_flags(), {name="user.media"}) then
+      print("set nice", proc)
+      proc:set_ioprio(7, ulatency.IOPRIO_CLASS_BE)
+    end
+
+    rv = ulatency.filter_rv(ulatency.FILTER_STOP)
+    return rv
+  end
+}
+
+ulatency.register_filter(MediaIO)
