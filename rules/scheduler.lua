73277ad90b888d5e8182bcd48a10519cba0c672d
Daniel Poelzleithner <poelzi@poelzi.org>
2011-02-20 10:01:11 +0100

    remove cruft from scheduler
    old libcgroup code, will be removed

--- rules/scheduler.lua
+++ rules/scheduler.lua
@@ -11,27 +11,6 @@ require("posix")
 u_groups = {}
 
 
---[[
-
-FIXME tests 
-
-grp = CGroup.new("bla/blubb")
-print("grp", grp)
-print(grp:path())
-print(grp:parent())
-grp:set_value("cpu.shares", 1302)
-grp:commit()
-pprint(grp:get_tasks())
-grp:add_task(1, true)
-pprint(grp:get_tasks())
-
-]]--
-
-function get_user_group(uid)
-  return CGroup.new("u_"..tostring(uid))
-end
-
-
 local UL_PID = posix.getpid()["pid"]
 
 ul_group_cpu = CGroup.new("s_ul", { ["cpu.shares"]="500"}, "cpu")
@@ -290,65 +269,5 @@ function Scheduler:get_config()
   return nil
 end
 
-
-function byby()
-  ulatency.quit_daemon()
-
-end
-
---ulatency.add_timeout(byby, 100000)
-
+-- register scheduler
 ulatency.scheduler = Scheduler
-
-
---[[
-
--- OLD libcgroup crap
-
-function mk_group(name)
-  grp = cgroups.new_cgroup("/"..name)
-  cpu = grp:add_controller("cpu")
-  print("cpu", cpu)
-  cpu:add_value("cpu.shares", 2048)
-  return grp
-end
-
-function apply(srcgroup)
-  print(srcgroup:get_name())
-  grp = cgroups.new_cgroup(srcgroup:get_name())
-  rv = grp:get_cgroup()
-  if(rv ~= 0) then
-    print("can't read kernel", rv)
-  end
-  
-  rv = grp:copy_from(srcgroup)
-  if(rv ~= 0) then
-    print("can't copy source", rv)
-  end
-  rv = grp:modify_cgroup()
-  if (rv ~= 0) then
-    print("can't mod croups", rv)
-  end
-
-end
-
-
---u_groups[""] = mk_group("")
-print("1")
---u_groups["s_daemon"] = root_group:create_cgroup_from_parent("/daemon")
-u_groups["s_daemon"] = mk_group("s_daemon")
-print("2")
-
-
-function get_user_group(uid)
-  name = "u_"..tostring(uid)
-  grp = u_groups[name]
-  if not grp then
-    grp = mk_group(name)
-    u_groups[name] = grp
-    grp:create_cgroup()
-  end
-  return grp
-end
-
---]]
