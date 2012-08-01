e087cbc86bb4ae3dd550a8408f6a7746f69813d6
Petr Gajdůšek <gajdusek.petr@centrum.cz>
2012-08-01 03:10:52 +0200

    Update documentation (including docs for commit fixing issue29)

--- rules/zz_fixers.lua
+++ rules/zz_fixers.lua
@@ -1,7 +1,18 @@
 -- last rules to execute, they can depend on flags set by previous rules
 
+--! @file zz_fixers.lua
+--! Implements the filters that manipulate processes flagged by previous rules.
+--! @ingroup lua_FLAG_FILTERS
+
+--! @class MediaIO
+--! @brief This filter sets real-time I/O priority for processes tagged with `{name="user-media"}` flag.
+--! @implements __FILTER
+--! @ingroup lua_FLAG_FILTERS
 MediaIO = {
+  --! @public @memberof MediaIO
   name = "MediaIO",
+  --! @return `ulatency.filter_rv(ulatency.FILTER_STOP)`
+  --! @public @memberof MediaIO
   check = function(self, proc)
     -- we give processes marked with media flags good io prio
     if ulatency.find_flag(proc:list_flags(), {name="user.media"}) then
