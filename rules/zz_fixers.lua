dea06defea7dfdb38cada9370dfb616d728da03d
Petr Gajdůšek <gajdusek.petr@centrum.cz>
2012-08-13 13:53:56 +0200

    fix MediaIO filter: check for inherited user.media flags

--- rules/zz_fixers.lua
+++ rules/zz_fixers.lua
@@ -15,7 +15,7 @@ MediaIO = {
   --! @public @memberof MediaIO
   check = function(self, proc)
     -- we give processes marked with media flags good io prio
-    if ulatency.find_flag(proc:list_flags(), {name="user.media"}) then
+    if ulatency.find_flag(proc:list_flags(true), {name="user.media"}) then
       proc:set_ioprio(7, ulatency.IOPRIO_CLASS_RT)
     end
 
