poelzi/ulatencyd

commit b0e7b10358d5d948a2894be941cf3015ea092c71

improve system_flags_changed/u_proc.changed variables setting on flag creation or removal
+ u_flag.urgent

(boolean) If TRUE, adding/removing the flag to/from an entity (u_proc or
system flags) will tag the entity as changed, i.e. u_proc.changed or
system_flags_changed variable will be set.

Default value for new flags is 1 (TRUE).

+ functions that add/removes a flag to/from entity (process or system
flags) get extra argument to specify if the entity should be tag as
changed, overriding default behaviour determined by flag.urgent.
Same applies for DBUS methods.

+ flag.inherit causes all children to be marked as changed

src/core.c
@@ -385,7 +385,7 @@ void u_proc_free(void *ptr) {
   }
   g_hash_table_destroy (proc->skip_filter);
 
-  u_flag_clear_all(proc);
+  u_flag_clear_all(proc, FALSE);
 
   u_proc_remove_child_nodes(proc);
 
@@ -1143,7 +1143,7 @@ int update_processes_run(PROCTAB *proctab, int full) {
     } else
         U_PROC_UNSET_STATE(proc, UPROC_BASIC);
 
-    if (! new_proc) u_flag_clear_timeout(proc, timeout);
+    if (! new_proc) u_flag_clear_timeout(proc, timeout, -1);
     updated = g_list_append(updated, proc);
 
     rv++;
@@ -1496,7 +1496,6 @@ int process_run_one(u_proc *proc, int update, int instant) {
   return TRUE;
 }
 
-
 /**
  * free flags
  * @ptr: #u_flag pointer
@@ -1534,6 +1533,7 @@ u_flag *u_flag_new(u_filter *source, const char *name) {
   rv->free_fnk = u_flag_free;
   rv->ref = 1;
   rv->source = source;
+  rv->urgent = 1;
 
   if(name) {
     rv->name = g_strdup(name);
@@ -1543,52 +1543,76 @@ u_flag *u_flag_new(u_filter *source, const char *name) {
 }
 
 /**
- * add flag to process
- * @arg proc #u_proc to add the flag to, or NULL for system flags
- * @arg flag #u_flag to add
+ * add flag to process or system flags
+ * @param proc #u_proc to add the flag to, or NULL for system flags
+ * @param flag #u_flag to add
+ * @param set_changed If 1, the `system_flags_changed` or `u_proc.changed`
+ * variable will be set. If 0, it won't. If -1 the behavior is determined by
+ * `u_flag.urgent` value.
  *
  * Adds a new flag to the u_proc or system flag list.
  *
  * @return boolean. TRUE on success.
  */
-int u_flag_add(u_proc *proc, u_flag *flag) {
+int u_flag_add(u_proc *proc, u_flag *flag, gint set_changed) {
   if(proc) {
     if(!g_list_find(proc->flags, flag)) {
       proc->flags = g_list_insert(proc->flags, flag, 0);
       INC_REF(flag);
     }
-    proc->changed = 1;
+    if (set_changed == 1 || flag->urgent) {
+      proc->changed = 1;
+      if (flag->inherit) {
+        u_proc_set_changed_flag_recursive(proc);
+      }
+    }
   } else {
     if(!g_list_find(system_flags, flag)) {
       system_flags = g_list_insert(system_flags, flag, 0);
       INC_REF(flag);
+      if (set_changed == 1 || flag->urgent) {
+        system_flags_changed = 1;
+      }
     }
   }
   return TRUE;
 }
 
 /**
- * delete flag from process
- * @arg proc #u_proc to remove the flag from, or NULL for system flags
- * @arg flag #u_flag to remove
+ * delete flag from process or system flags
+ * @param proc #u_proc to remove the flag from, or NULL for system flags
+ * @param flag #u_flag to remove
+ * @param set_changed If 1, the `system_flags_changed` or `u_proc.changed`
+ * variable will be set. If 0, it won't. If -1 the behavior is determined by
+ * `u_flag.urgent` value.
  *
  * Removes a flag from a process or system flags.
  *
  * @return boolean. TRUE on success.
  */
-int u_flag_del(u_proc *proc, u_flag *flag) {
+int u_flag_del(u_proc *proc, u_flag *flag, gint set_changed) {
   if(proc) {
-    if(g_list_index(proc->flags, flag) != -1) {
-      DEC_REF(flag);
+    if(g_list_index(proc->flags, flag) == -1) {
+      return FALSE;
+    }
+    if (set_changed == 1 || flag->urgent) {
+      proc->changed = 1;
+      if (flag->inherit) {
+        u_proc_set_changed_flag_recursive(proc);
+      }
     }
     proc->flags = g_list_remove(proc->flags, flag);
-    proc->changed = 1;
+    DEC_REF(flag);
     return TRUE;
   } else {
-    if(g_list_index(system_flags, flag) != -1) {
-      DEC_REF(flag);
+    if(g_list_index(system_flags, flag) == -1) {
+      return FALSE;
+    }
+    if (set_changed == 1 || flag->urgent) {
+      system_flags_changed = 1;
     }
     system_flags = g_list_remove(system_flags, flag);
+    DEC_REF(flag);
     return TRUE;
   }
   return FALSE;
@@ -1627,24 +1651,47 @@ static int u_flag_match_timeout(gconstpointer a, gconstpointer time) {
   return (flg->timeout > t);
 }
 
+// helper for u_proc_set_changed_flag_recursive / u_proc_clear_changed_flag_recursive
+static gboolean _g_node_proc_set_changed_flag(GNode *node, gpointer changed) {
+  u_proc *proc = node->data;
+  proc->changed = GPOINTER_TO_INT(changed);
+  return FALSE;
+}
+
+void u_proc_set_changed_flag_recursive(u_proc *proc) {
+  g_node_traverse(proc->node, G_PRE_ORDER, G_TRAVERSE_ALL, -1,
+      _g_node_proc_set_changed_flag, GUINT_TO_POINTER(1));
+}
+
+void u_proc_clear_changed_flag_recursive(u_proc *proc) {
+  g_node_traverse(proc->node, G_PRE_ORDER, G_TRAVERSE_ALL, -1,
+      _g_node_proc_set_changed_flag, GUINT_TO_POINTER(0));
+}
 
 #define CLEAR_BUILD(NAME, ARG, CMP) \
-int NAME (u_proc *proc, ARG ) { \
+int NAME (u_proc *proc, ARG, gint set_changed) { \
   GList *item; \
   int rv = 0; \
   while((item = CMP ) != NULL) { \
     if(proc) { \
+      if (set_changed == 1 || ((u_flag *)item)->urgent) { \
+        proc->changed = 1; \
+        if (((u_flag *)item)->inherit) { \
+            u_proc_set_changed_flag_recursive(proc); \
+        } \
+      } \
       proc->flags = g_list_remove_link (proc->flags, item); \
       DEC_REF(item->data); \
       item->data = NULL; \
-      proc->changed = 1; \
       rv++; \
       g_list_free(item); \
     } else { \
       system_flags = g_list_remove_link (system_flags, item); \
       DEC_REF(item->data); \
       item->data = NULL; \
-      system_flags_changed = 1; \
+      if (set_changed == 1 || ((u_flag *)item)->urgent) { \
+        system_flags_changed = 1; \
+      } \
       rv ++; \
       g_list_free(item); \
     } \
@@ -1660,17 +1707,26 @@ CLEAR_BUILD(u_flag_clear_flag, const void *var, g_list_find_custom(proc ? proc->
 
 CLEAR_BUILD(u_flag_clear_timeout, time_t tm, g_list_find_custom(proc ? proc->flags : system_flags, GUINT_TO_POINTER(tm), u_flag_match_timeout))
 
-int u_flag_clear_all(u_proc *proc) {
+int u_flag_clear_all(u_proc *proc, gint set_changed) {
   GList *item;
   int rv = 0;
+  gboolean change_children = FALSE;
   if(proc) {
     while((item = g_list_first(proc->flags)) != NULL) {
+      if (set_changed == 1 || ((u_flag *)item)->urgent) {
+        proc->changed = 1;
+        if (((u_flag *)item)->inherit) {
+          change_children = TRUE;
+        }
+      }
       proc->flags = g_list_remove_link (proc->flags, item);
       DEC_REF(item->data);
       item->data = NULL;
-      proc->changed = 1;
       rv++;
       g_list_free(item);
+    }
+    if (change_children) {
+      u_proc_set_changed_flag_recursive(proc);
     }
     g_list_free(proc->flags);
   } else {
@@ -1680,7 +1736,9 @@ int u_flag_clear_all(u_proc *proc) {
       item->data = NULL;
       g_list_free(item);
       rv++;
-      system_flags_changed = 1;
+      if (set_changed == 1 || ((u_flag *)item)->urgent) {
+        system_flags_changed = 1;
+      }
     }
     g_list_free(system_flags);
   }
@@ -1895,7 +1953,7 @@ int iterate(gpointer rv) {
           tparse, tfilter, tscheduler, (tparse + tfilter + tscheduler));
 
   g_timer_start(timer);
-  u_flag_clear_timeout(NULL, timeout);
+  u_flag_clear_timeout(NULL, timeout, -1);
   iteration += 1;
   g_log(G_LOG_DOMAIN, G_LOG_LEVEL_DEBUG, "start iteration %d:", iteration);
   update_caches();

modules/simplerules.c
@@ -351,10 +351,12 @@ void simple_add_flag(u_filter *filter, u_proc *proc, struct simple_rule *rule) {
     nf->value       = t->value;
     nf->threshold   = t->threshold;
     nf->inherit     = t->inherit;
+    if(t->urgent)
+        nf->urgent = t->urgent;
 
     u_trace("add flag %s to %d", nf->name, proc->pid); 
 
-    u_flag_add(proc, nf);
+    u_flag_add(proc, nf, -1);
     DEC_REF(nf);
 }
 

src/dbus.c
@@ -105,6 +105,7 @@ DBUS_INTROSPECT_1_0_XML_DOCTYPE_DECL_NODE
 "      <arg type=\"x\" name=\"value\" direction=\"in\" />\n"
 "      <arg type=\"x\" name=\"threshold\" direction=\"in\" />\n"
 "      <arg type=\"b\" name=\"inherit\" direction=\"in\" />\n"
+"      <arg type=\"b\" name=\"urgent\" direction=\"in\" />\n"
 "    </method>\n"
 "    <method name=\"addSystemFlag\">\n"
 "      <arg type=\"s\" name=\"name\" direction=\"in\" />\n"
@@ -113,6 +114,7 @@ DBUS_INTROSPECT_1_0_XML_DOCTYPE_DECL_NODE
 "      <arg type=\"i\" name=\"priority\" direction=\"in\" />\n"
 "      <arg type=\"x\" name=\"value\" direction=\"in\" />\n"
 "      <arg type=\"x\" name=\"threshold\" direction=\"in\" />\n"
+"      <arg type=\"b\" name=\"urgent\" direction=\"in\" />\n"
 "      <arg type=\"b\" name=\"run_iteration\" direction=\"in\" />\n"
 "    </method>\n"
 "    <method name=\"listFlags\">\n"
@@ -463,7 +465,7 @@ static DBusHandlerResult dbus_system_handler(DBusConnection *c, DBusMessage *m, 
 
         uint64_t tpid, ttid, timeout;
         int64_t priority, value, threshold;
-        uint32_t inherit;
+        uint32_t inherit, urgent;
         u_flag *flag;
         char *name, *reason;
 
@@ -477,6 +479,7 @@ static DBusHandlerResult dbus_system_handler(DBusConnection *c, DBusMessage *m, 
                                       DBUS_TYPE_INT64,  &value,
                                       DBUS_TYPE_INT64,  &threshold,
                                       DBUS_TYPE_BOOLEAN,&inherit,
+                                      DBUS_TYPE_BOOLEAN,&urgent,
                                       DBUS_TYPE_INVALID) ||
             !dbus_message_iter_init (m, &imsg))
               PUSH_ERROR(DBUS_ERROR_INVALID_ARGS, "wrong arguments")
@@ -502,8 +505,9 @@ static DBusHandlerResult dbus_system_handler(DBusConnection *c, DBusMessage *m, 
         flag->value = value;
         flag->threshold = threshold;
         flag->inherit = inherit;
+        flag->urgent = urgent;
 
-        u_flag_add(proc, flag);
+        u_flag_add(proc, flag, -1);
         DEC_REF(flag);
 
         ret = dbus_message_new_method_return(m);
@@ -514,6 +518,7 @@ static DBusHandlerResult dbus_system_handler(DBusConnection *c, DBusMessage *m, 
         int64_t priority, value, threshold;
         u_flag *flag;
         gboolean run_iteration;
+        uint32_t urgent;
         char *name, *reason;
 
         if (!dbus_message_get_args(m, &error,
@@ -523,6 +528,7 @@ static DBusHandlerResult dbus_system_handler(DBusConnection *c, DBusMessage *m, 
                                       DBUS_TYPE_INT32,   &priority,
                                       DBUS_TYPE_INT64,   &value,
                                       DBUS_TYPE_INT64,   &threshold,
+                                      DBUS_TYPE_BOOLEAN, &urgent,
                                       DBUS_TYPE_BOOLEAN, &run_iteration,
                                       DBUS_TYPE_INVALID) ||
             !dbus_message_iter_init (m, &imsg))
@@ -539,15 +545,17 @@ static DBusHandlerResult dbus_system_handler(DBusConnection *c, DBusMessage *m, 
         flag->priority = priority;
         flag->value = value;
         flag->threshold = threshold;
+        flag->urgent = urgent;
 
-        u_flag_add(NULL, flag);
+        u_flag_add(NULL, flag, -1);
         DEC_REF(flag);
-        system_flags_changed = 1;
 
         g_message("DBUS: added system flag: name=\"%s\", reason=\"%s\"", name, reason);
 
-        if (run_iteration)
-            g_timeout_add(0, iterate, GUINT_TO_POINTER(0));
+        if (run_iteration) {
+          system_flags_changed = 1;
+          g_timeout_add(0, iterate, GUINT_TO_POINTER(0));
+        }
 
         ret = dbus_message_new_method_return(m);
         goto finish;
@@ -587,9 +595,9 @@ static DBusHandlerResult dbus_system_handler(DBusConnection *c, DBusMessage *m, 
             PUSH_ERROR(DBUS_ERROR_ACCESS_DENIED, "access denied")
 
         if(is2) {
-            u_flag_clear_flag(proc, (void *)id);
+            u_flag_clear_flag(proc, (void *)id, TRUE);
         } else {
-            u_flag_clear_source(proc, U_DBUS_POINTER);
+            u_flag_clear_source(proc, U_DBUS_POINTER, TRUE);
         }
 
         ret = dbus_message_new_method_return(m);

src/docs/doxygen/U_PROC.ldox
@@ -163,39 +163,49 @@ function U_PROC:list_flags(recursive)
 end
 
 --! @brief Adds the flag #u_flag to the process #U_PROC.
---! @note The #U_PROC is marked as changed, see `#U_PROC.changed`.
 --! @param flag a #u_flag to add
+--! @param (optional) set_proc_changed (boolean) If TRUE and a flag is removed,
+--! the `U_PROC.changed` will be set. If FALSE, the `#U_PROC.changed` won't be set.
+--! Default behaviour is determined by `u_flag.urgent`.
 --! @public @memberof U_PROC
-function U_PROC:add_flag(flag)
+function U_PROC:add_flag(flag, set_proc_changed)
 end
 
 --! @brief Removes a flag #u_flag from a process #U_PROC.
---! @note The #U_PROC is marked as changed, see `#U_PROC.changed`.
 --! @param flag a #u_flag to delete
+--! @param (optional) set_proc_changed (boolean) If TRUE and a flag is removed,
+--! the `U_PROC.changed` will be set. If FALSE, the `#U_PROC.changed` won't be set.
+--! Default behaviour is determined by `u_flag.urgent`.
 --! @public @memberof U_PROC
-function U_PROC:del_flag(flag)
+function U_PROC:del_flag(flag, set_proc_changed)
 end
 
 --! @brief Removes flags with the `name` from a process #U_PROC.
---! @note If a flag is removed, the #U_PROC is marked as changed, see `#U_PROC.changed`.
 --! @param name of #u_flags to delete
+--! @param (optional) set_proc_changed (boolean) If TRUE and a flag is removed,
+--! the `U_PROC.changed` will be set. If FALSE, the `#U_PROC.changed` won't be set.
+--! Default behaviour is determined by `u_flag.urgent`.
 --! @public @memberof U_PROC
-function U_PROC:clear_flag_name(name)
+function U_PROC:clear_flag_name(name, set_proc_changed)
 end
 
 --! @brief From a process #U_PROC removes all flags that was created by current lua thread.
 --! Specifically, all flags created with same lua state will be removed.
 --! There is one main lua state and additional lua states for every lua thread created by
 --! ulatency.add_timeout() and ulatency.register_filter().
---! @note If a flag is removed, the #U_PROC is marked as changed, see `#U_PROC.changed`.
+--! @param (optional) set_proc_changed (boolean) If TRUE and a flag is removed,
+--! the `U_PROC.changed` will be set. If FALSE, the `#U_PROC.changed` won't be set.
+--! Default behaviour is determined by `u_flag.urgent`.
 --! @public @memberof U_PROC
-function U_PROC:clear_flag_source()
+function U_PROC:clear_flag_source(set_proc_changed)
 end
 
 --! @brief Removes all flags from a process #U_PROC.
---! @note If a flag is removed, the #U_PROC is marked as changed, see `#U_PROC.changed`.
+--! @param (optional) set_proc_changed (boolean) If TRUE and a flag is removed,
+--! the `U_PROC.changed` will be set. If FALSE, the `#U_PROC.changed` won't be set.
+--! Default behaviour is determined by `u_flag.urgent`.
 --! @public @memberof U_PROC
-function U_PROC:clear_flag_all()
+function U_PROC:clear_flag_all(set_proc_changed)
 end
 --! @} End of "process flags manipulation"
 

src/docs/doxygen/u_flag.dox
@@ -11,7 +11,7 @@
  * @ingroup lua_PSEUDO lua_FLAGS
  */
 
-typedef struct _FLAG {
+typedef struct U_FLAG {
   U_HEAD;
   /**
    * @details This pointer is passed to the flag constructor `u_flag_new()` and is later used by
@@ -31,4 +31,10 @@ typedef struct _FLAG {
   int64_t        value;         //!< custom data: value
   int64_t        threshold;     //!< custom data: threshold
   uint32_t       inherit : 1;      //!< will apply to all children
+  /**
+   * @details (boolean) If TRUE, adding/removing the flag to/from an entity (u_proc or system flags) will
+   * tag the entity as changed, i.e. u_proc.changed or system_flags_changed variable will be set.
+   * Default value for new flags is 1 (TRUE).
+   */
+  uint32_t       urgent: 1;     //!< adding/removing the flag will tag the holder as changed
 } u_flag;

src/docs/doxygen/ulatency.ldox
@@ -305,39 +305,54 @@ function ulatency.list_flags()
 end
 
 --! Adds the flag #u_flag to the system flags.
---! @note The `system_flags_changed` variable is **not touched**.
 --! @param flag a #u_flag to add
+--! @param (optional) set_flags_changed (boolean) If TRUE,
+--! the `system_flags_changed` variable will be set. If FALSE,
+--! it won't. Default behaviour is determined by
+--! `u_flag.urgent`. See `ulatency.set_flags_changed()`.
 --! @public @memberof ulatency
-function ulatency.add_flag(flag)
+function ulatency.add_flag(flag, set_flags_changed)
 end
 
 --! @brief Removes a system flag #u_flag.
---! @note The `system_flags_changed` variable is **not touched**.
 --! @param flag a #u_flag to delete
+--! @param (optional) set_flags_changed (boolean) If TRUE and a flag is removed,
+--! the `system_flags_changed` variable will be set. If FALSE,
+--! the `system_flags_changed` won't be set. Default behaviour is determined by
+--! `u_flag.urgent`. See `ulatency.set_flags_changed()`.
 --! @public @memberof ulatency
-function ulatency.del_flag(flag)
+function ulatency.del_flag(flag, set_flags_changed)
 end
 
 --! @brief Removes system flags with the `name`.
---! @note If a flag is removed, the `system_flags_changed` variable is set, see ulatency.set_flags_changed().
 --! @param name System flags (#u_flag instances) with this name will be deleted.
+--! @param (optional) set_flags_changed (boolean) If TRUE and a flag is removed,
+--! the `system_flags_changed` variable will be set. If FALSE,
+--! the `system_flags_changed` won't be set. Default behaviour is determined by
+--! `u_flag.urgent`. See `ulatency.set_flags_changed()`.
 --! @public @memberof ulatency
-function ulatency.clear_flag_name(name)
+function ulatency.clear_flag_name(name, set_flags_changed)
 end
 
 --! @brief Removes all system flags that was created by current lua thread.
 --! Specifically, all flags created with same lua state will be removed.
 --! There is one main lua state and additional lua states for every lua thread created by
 --! ulatency.add_timeout() and ulatency.register_filter().
---! @note If a flag is removed, the `system_flags_changed` variable is set, see ulatency.set_flags_changed().
+--! @param (optional) set_flags_changed (boolean) If TRUE and a flag is removed,
+--! the `system_flags_changed` variable will be set. If FALSE,
+--! the `system_flags_changed` won't be set. Default behaviour is determined by
+--! `u_flag.urgent`. See `ulatency.set_flags_changed()`.
 --! @public @memberof ulatency
-function ulatency.clear_flag_source()
+function ulatency.clear_flag_source(set_flags_changed)
 end
 
 --! Removes all system flags.
---! @note If a flag is removed, the `system_flags_changed` variable is set, see ulatency.set_flags_changed().
+--! @param (optional) set_flags_changed (boolean) If TRUE and a flag is removed,
+--! the `system_flags_changed` variable will be set. If FALSE,
+--! the `system_flags_changed` won't be set. Default behaviour is determined by
+--! `u_flag.urgent`. See `ulatency.set_flags_changed()`.
 --! @public @memberof ulatency
-function ulatency.clear_flag_all()
+function ulatency.clear_flag_all(set_flags_changed)
 end
 
 --! Returns the value of the `system_flags_changed` variable.
@@ -350,17 +365,10 @@ end
 
 --! Sets the value of the `system_flags_changed` variable.
 --! @param value a boolean
---! @note The `system_flags_changed` is automatically set if:
---! - Some of the system flags has timed out.
---! - Some of the system flags has been removed as result of calling `ulatency.clear_flag_name()`,
---!   `ulatency.clear_flag_source()` or `ulatency.clear_flag_all()`.
---! @note The `system_flags_changed` is not set by:
---! - `ulatency.add_flag()` or `ulatency.del_flag()`
---! - changing a flag values
---! @note The `system_flags_changed` is cleared after every iteration, see ulatency.run_iteration().
 --! @note `Scheduler::all()` will normally schedule only processes that are marked as changed (see `U_PROC.clear_changed()`
---! for description of what processes are considered changed). If you want to force rescheduling of all processes, e.g.
---! because you have added some important system flag, set the `system_flags_changed` manually.
+--! for description of what processes are considered changed). If you want to force rescheduling of all processes,
+--! set the `system_flags_changed` manually.
+--! @note The `system_flags_changed` is cleared after every iteration, see ulatency.run_iteration().
 --! @relatesalso Scheduler
 --! @public @memberof ulatency
 function ulatency.set_flags_changed(value)

src/lua_binding.c
@@ -728,8 +728,13 @@ static int _u_proc_get_current_task_pids (lua_State *L) {
 static int u_proc_add_flag (lua_State *L) {
   u_proc *proc = check_u_proc(L, 1);
   u_flag *flag = check_u_flag(L, 2);
+  gint set_proc_changed = -1;
 
-  lua_pushinteger(L, u_flag_add(proc, flag));
+  if (lua_isboolean(L, 3)) {
+    set_proc_changed = lua_toboolean(L, 3);
+  }
+
+  lua_pushinteger(L, u_flag_add(proc, flag, set_proc_changed));
 
   return 1;
 }
@@ -737,8 +742,13 @@ static int u_proc_add_flag (lua_State *L) {
 static int u_proc_del_flag (lua_State *L) {
   u_proc *proc = check_u_proc(L, 1);
   u_flag *flag = check_u_flag(L, 2);
+  gint set_proc_changed = -1;
 
-  lua_pushinteger(L, u_flag_del(proc, flag));
+  if (lua_isboolean(L, 3)) {
+    set_proc_changed = lua_toboolean(L, 3);
+  }
+
+  lua_pushinteger(L, u_flag_del(proc, flag, set_proc_changed));
 
   return 1;
 }
@@ -746,32 +756,54 @@ static int u_proc_del_flag (lua_State *L) {
 static int u_proc_clear_flag_name (lua_State *L) {
   u_proc *proc = check_u_proc(L, 1);
   const char *name = luaL_checkstring(L, 2);
+  gint set_proc_changed = -1;
 
-  u_flag_clear_name(proc, name);
+  if (lua_isboolean(L, 3)) {
+     set_proc_changed = lua_toboolean(L, 3);
+  }
+
+  u_flag_clear_name(proc, name, set_proc_changed);
 
   return 0;
 }
 
 static int u_proc_clear_flag_source (lua_State *L) {
   u_proc *proc = check_u_proc(L, 1);
+  gint set_proc_changed = -1;
 
-  u_flag_clear_source(proc, L);
+  if (lua_isboolean(L, 2)) {
+     set_proc_changed = lua_toboolean(L, 2);
+  }
+
+  u_flag_clear_source(proc, L, set_proc_changed);
 
   return 0;
 }
 
 static int u_proc_clear_flag_all (lua_State *L) {
   u_proc *proc = check_u_proc(L, 1);
+  gint set_proc_changed = -1;
 
-  u_flag_clear_all(proc);
+  if (lua_isboolean(L, 2)) {
+     set_proc_changed = lua_toboolean(L, 2);
+  }
+
+  u_flag_clear_all(proc, set_proc_changed);
 
   return 0;
 }
 
 static int u_proc_clear_changed (lua_State *L) {
   u_proc *proc = check_u_proc(L, 1);
+  gboolean recursive = FALSE;
+  if(lua_isboolean(L, 2)) {
+     recursive = lua_toboolean(L, 2);
+   }
 
   proc->changed = 0;
+  if (recursive) {
+    u_proc_clear_changed_flag_recursive(proc);
+  }
 
   return 0;
 }
@@ -1491,6 +1523,7 @@ static int u_flag_index (lua_State *L)
   PUSH_CHR(reason)
   PUSH_INT(value)
   PUSH_INT(threshold)
+  PUSH_INT(urgent)
   if(!strcmp(key, "is_source")) {
     lua_pushboolean(L, flag->source == L);
   }
@@ -1504,13 +1537,12 @@ static int u_flag_newindex (lua_State *L)
 
   PULL_CHR(name)
   PULL_CHR(reason)
-
   PULL_INT(inherit)
   PULL_INT(priority)
   PULL_INT(timeout)
-  //PULL_INT(reason)
   PULL_INT(value)
   PULL_INT(threshold)
+  PULL_INT(urgent)
   return 0;
 }
 
@@ -1545,6 +1577,7 @@ static int l_flag_new (lua_State *L)
     CHK_N_SET(reason, g_strdup(lua_tostring(L, -1)) )
     CHK_N_SET(value, lua_tointeger(L, -1))
     CHK_N_SET(threshold, lua_tointeger(L, -1))
+    CHK_N_SET(urgent, lua_toboolean(L, -1))
     return 1;
   }
   
@@ -1608,38 +1641,63 @@ static int u_sys_list_flags (lua_State *L) {
 
 static int u_sys_add_flag (lua_State *L) {
   u_flag *flag = check_u_flag(L, 1);
+  gint set_flags_changed = -1;
 
-  lua_pushinteger(L, u_flag_add(NULL, flag));
+  if (lua_isboolean(L, 2)) {
+    set_flags_changed = lua_toboolean(L, 2);
+  }
+
+  lua_pushinteger(L, u_flag_add(NULL, flag, set_flags_changed));
 
   return 1;
 }
 
 static int u_sys_del_flag (lua_State *L) {
   u_flag *flag = check_u_flag(L, 1);
+  gint set_flags_changed = -1;
 
-  lua_pushinteger(L, u_flag_del(NULL, flag));
+  if (lua_isboolean(L, 2)) {
+    set_flags_changed = lua_toboolean(L, 2);
+  }
+
+  lua_pushinteger(L, u_flag_del(NULL, flag, set_flags_changed));
 
   return 1;
 }
 
 static int u_sys_clear_flag_name (lua_State *L) {
   const char *name = luaL_checkstring(L, 1);
+  gint set_flags_changed = -1;
 
-  u_flag_clear_name(NULL, name);
+  if (lua_isboolean(L, 2)) {
+    set_flags_changed = lua_toboolean(L, 2);
+  }
+
+  u_flag_clear_name(NULL, name, set_flags_changed);
 
   return 0;
 }
 
 static int u_sys_clear_flag_source (lua_State *L) {
+  gint set_flags_changed = -1;
 
-  u_flag_clear_source(NULL, L);
+  if (lua_isboolean(L, 1)) {
+    set_flags_changed = lua_toboolean(L, 1);
+  }
+
+  u_flag_clear_source(NULL, L, set_flags_changed);
 
   return 0;
 }
 
 static int u_sys_clear_flag_all (lua_State *L) {
+  gint set_flags_changed = -1;
 
-  u_flag_clear_all(NULL);
+  if (lua_isboolean(L, 2)) {
+    set_flags_changed = lua_toboolean(L, 2);
+  }
+
+  u_flag_clear_all(NULL, set_flags_changed);
 
   return 0;
 }

src/ulatency.h
@@ -256,7 +256,7 @@ typedef struct _filter {
   NONE = 0,
   REPLACE_SOURCE,
   ADD,
-} FLAG_BEHAVIOUR;
+} FLAG_TIMEOUT_BEHAVIOUR;
 */
 typedef struct _FLAG {
   U_HEAD;
@@ -270,19 +270,20 @@ typedef struct _FLAG {
   int64_t        value;         //!< custom data: value
   int64_t        threshold;     //!< custom data: threshold
   uint32_t       inherit : 1;      //!< will apply to all children
+  uint32_t       urgent: 1;     //!< adding/removing the flag will tag the holder (u_proc or system flags) as changed
 } u_flag;
 
 
 u_flag *u_flag_new(u_filter *source, const char *name);
 void u_flag_free(void *data);
 
-int u_flag_add(u_proc *proc, u_flag *flag);
-int u_flag_del(u_proc *proc, u_flag *flag);
-int u_flag_clear_source(u_proc *proc, const void *source);
-int u_flag_clear_name(u_proc *proc, const char *name);
-int u_flag_clear_all(u_proc *proc);
-int u_flag_clear_flag(u_proc *proc, const void *flag);
-int u_flag_clear_timeout(u_proc *proc, time_t timeout);
+int u_flag_add(u_proc *proc, u_flag *flag, gint set_changed);
+int u_flag_del(u_proc *proc, u_flag *flag, gint set_changed);
+int u_flag_clear_source(u_proc *proc, const void *source, gint set_changed);
+int u_flag_clear_name(u_proc *proc, const char *name, gint set_changed);
+int u_flag_clear_all(u_proc *proc, gint set_changed);
+int u_flag_clear_flag(u_proc *proc, const void *flag, gint set_changed);
+int u_flag_clear_timeout(u_proc *proc, time_t timeout, gint set_changed);
 
 struct u_cgroup {
   struct cgroup *group;
@@ -431,6 +432,8 @@ enum ENSURE_UPDATE {
 };
 
 int u_proc_ensure(u_proc *proc, enum ENSURE_WHAT what, enum ENSURE_UPDATE update);
+void u_proc_set_changed_flag_recursive(u_proc *proc);
+void u_proc_clear_changed_flag_recursive(u_proc *proc);
 GList *u_proc_list_flags (u_proc *proc, gboolean recrusive);
 GArray *u_proc_get_current_task_pids(u_proc *proc);
 

commit a8ecfe8befa9503d08ac539cfc7435a37953d84e

fix u_flag_clear_*() bug introduced in b0e7b10

src/core.c
@@ -1674,9 +1674,9 @@ int NAME (u_proc *proc, ARG, gint set_changed) { \
   int rv = 0; \
   while((item = CMP ) != NULL) { \
     if(proc) { \
-      if (set_changed == 1 || ((u_flag *)item)->urgent) { \
+      if (set_changed == 1 || ((u_flag *)item->data)->urgent) { \
         proc->changed = 1; \
-        if (((u_flag *)item)->inherit) { \
+        if (((u_flag *)item->data)->inherit) { \
             u_proc_set_changed_flag_recursive(proc); \
         } \
       } \
@@ -1689,7 +1689,7 @@ int NAME (u_proc *proc, ARG, gint set_changed) { \
       system_flags = g_list_remove_link (system_flags, item); \
       DEC_REF(item->data); \
       item->data = NULL; \
-      if (set_changed == 1 || ((u_flag *)item)->urgent) { \
+      if (set_changed == 1 || ((u_flag *)item->data)->urgent) { \
         system_flags_changed = 1; \
       } \
       rv ++; \
@@ -1713,9 +1713,9 @@ int u_flag_clear_all(u_proc *proc, gint set_changed) {
   gboolean change_children = FALSE;
   if(proc) {
     while((item = g_list_first(proc->flags)) != NULL) {
-      if (set_changed == 1 || ((u_flag *)item)->urgent) {
+      if (set_changed == 1 || ((u_flag *)item->data)->urgent) {
         proc->changed = 1;
-        if (((u_flag *)item)->inherit) {
+        if (((u_flag *)item->data)->inherit) {
           change_children = TRUE;
         }
       }
@@ -1736,7 +1736,7 @@ int u_flag_clear_all(u_proc *proc, gint set_changed) {
       item->data = NULL;
       g_list_free(item);
       rv++;
-      if (set_changed == 1 || ((u_flag *)item)->urgent) {
+      if (set_changed == 1 || ((u_flag *)item->data)->urgent) {
         system_flags_changed = 1;
       }
     }