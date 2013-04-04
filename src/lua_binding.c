e99c806f89778196e2e99aca0e0804ded7c1f4f9
Petr Gajdůšek <gajdusek.petr@centrum.cz>
2013-04-04 09:46:52 +0200

    lua bindings: rename U_PROC.usession_{id,is_active} to U_PROC.session_*

--- src/lua_binding.c
+++ src/lua_binding.c
@@ -1260,10 +1260,10 @@ static int u_proc_index (lua_State *L)
   } else if(!strcmp(key, "active_pos" )) {
     lua_pushinteger(L, get_active_pos(proc));
     return 1;
-  } else if(!strcmp(key, "usession_id" )) {
+  } else if(!strcmp(key, "session_id" )) {
     lua_pushinteger(L, u_session_id_find_by_proc(proc));
     return 1;
-  } else if(!strcmp(key, "usession_is_active" )) {
+  } else if(!strcmp(key, "session_is_active" )) {
     USession *sess = u_session_find_by_proc(proc);
     lua_pushboolean(L, sess ? sess->active : 0);
     return 1;
