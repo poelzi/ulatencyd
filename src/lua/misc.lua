65800c86d6f995ec71b8cc0d5abf14a20e709d49
Petr Gajdůšek <gajdusek.petr@centrum.cz>
2014-02-27 11:39:56 +0100

    Improve handling of errors for tasks that couldn't be added to a cgroup
    - If moving fails for a real-time task on kernel without
      CONFIG_RT_GROUP_SCHED, print the warning only once; make the warning
      more verbose.
    
    - Do not warn if kernel process cannot be moved into a cgroup;
      kernel processes have euid=0, gid=0 and vm_size=0. Block such
      processes from future shceduler run.
    
    - But print warning (only once) if such kernel task has been already
      moved before. And do not block it from scheduling.
    
    - Include more information about task/process in the message.

--- src/lua/misc.lua
+++ src/lua/misc.lua
@@ -57,58 +57,182 @@ function mkdirp(path)
   return 0
 end
 
+do
+  -- seen move tasks errors that should be reported only once
+  -- { ['cpu.rt_task'] = true } failed moving of real-time processes on non
+  --                            CONFIG_RT_GROUP_SCHED kernel
+  local reported_move_task_errors = {}
 
---! @brief Log error after writing to sysfs failed: decides whether the error should be logged,
---! the log level and composes the error message.
---! @param filepath The file full path
---! @param value A value that trigger the error
---! @param err Textual representation of the error
---! @param err_code The error code (a number)
-function sysfs_write_error(filepath, value, err, err_code)
-
-  local s,e = string.find(filepath, CGROUP_ROOT, 1, true)
-  if s == 1 then
-
-    -- error while writing to a cgroup subsystem
-    local fields = string.split(string.sub(filepath, e+1), '/')
-    if #fields >= 2 then
-      local subsys = fields[1]
-      local file = fields[#fields]
-      local cgr_name = #fields >= 3 and table.concat(fields, '/', 2, #fields-1) or ""
-      local cgr = CGroup.get_group(subsys ..'/'.. cgr_name)
-      if cgr then
-        -- error while adding task to a cgroup
-        if file == "tasks" then
-          local proc = ulatency.get_pid(value)
-          -- no such process; ignore this error
-          if err_code == 3 then
-            return
-          end
-          -- error while moving rt task between cgroups in cpu subsystem on kernel without CONFIG_RT_GROUP_SCHED
-          if cgr.tree == "cpu" and err_code == 22 and posix.access(cgr:path("cpu.rt_runtime_us")) ~= 0 then
-            local task = ulatency.get_tid(value)
-            if task and (task.sched == ulatency.SCHED_RR or task.sched == ulatency.SCHED_FIFO) then
-              ulatency.log_debug(string.format(
-                "Task (tid: %s, RT sched.) couldn't be moved to %s (%d: %s) (probably kernel w/o CONFIG_RT_GROUP_SCHED)",
-                tostring(value), tostring(cgr), err_code, err
-              ))
-              return
-            end
-          end
-          -- other error
-          ulatency.log_warning(string.format(
-                "Task (tid: %s, cmdfile: %s, exe: %s) couldn't be moved to %s (%d: %s)",
-                tostring(value), proc.cmdfile or "NONE", proc.exe or 'NONE',
-                tostring(cgr), err_code, err))
-          return
-        end
+  --! @brief Log error after writing to sysfs failed: decides whether the error
+  --! should be logged, the log level and composes the error message.
+  --! @param filepath The file full path
+  --! @param value A value that trigger the error
+  --! @param err Textual representation of the error
+  --! @param err_code The error code (a number)
+  function sysfs_write_error(filepath, value, err, err_code)
+    -- is writing to file inside CGROUP_ROOT ?
+    local cgr_name, cgr, subsys, file
+    local s,e = string.find(filepath, CGROUP_ROOT, 1, true)
+    if s == 1 then
+      -- yes, so this is error while writing to a cgroup subsystem
+      local fields = string.split(string.sub(filepath, e+1), '/')
+      if #fields >= 2 then
+        subsys = fields[1]
+        file = fields[#fields]
+        cgr_name = #fields >= 3 and
+                   table.concat(fields, '/', 2, #fields-1) or ""
+        cgr = CGroup.get_group(subsys ..'/'.. cgr_name)
       end
     end
-  end
 
-  ulatency.log_warning(string.format("can't write string '%s' into %s: (%d) %s",tostring(value),filepath,err_code,err))
-end
+    ----------------------------------------------------------------------------
+    -- Begin handling error: Task could not be added to a cgroup
+    ----------------------------------------------------------------------------
 
+    if cgr and file == "tasks" then
+      -- ignore errors "no such process"
+      if err_code == 3 then
+        return
+      end
+      -- try to get both U_PROC and U_TASK instance as we do not know if
+      -- the moving process is a thread (task) or thread leader (process)
+      local proc = ulatency.get_pid(value)
+      local task = ulatency.get_tid(value)
+      -- skip if the task should be reported only once and
+      -- this already happened
+      if task and task.data.do_not_report_move_errors and
+                  task.data.do_not_report_move_errors[cgr.tree]
+      then
+        return
+      end
+
+      --- Special cases
+
+      -- additional info to be logged
+      local props = {} -- additional task/proc properties
+      local info = {}  -- texts to be displayed after the message
+      local log_level = ulatency.LOG_LEVEL_WARNING
+
+      -- if kernel process failed to be moved and was never moved before
+      -- in any cgroup subsystem then skip future scheduling
+      if task and task.euid == 0 and task.egid == 0 and
+            task.vm_size == 0
+      then
+        if not task.data.do_not_report_move_errors then
+          task.data.do_not_report_move_errors = {}
+        end
+        props[#props+1] = "KERNEL PROCESS"
+        if proc then
+          -- disable move errors reporting of such subsystems where the
+          -- process is in root cgroup.
+          local cgroups = {}
+          local in_root_cgroups = true
+          for _, subsys in ipairs(ulatency.get_cgroup_subsystems()) do
+            if ulatency.has_cgroup_subsystem(subsys) then
+              local cgr_path = proc:get_cgroup(subsys)
+              if cgr_path and cgr_path ~= "/" then
+                cgroups[#cgroups+1] = subsys..":"..cgr_path
+                in_root_cgroups = false
+              elseif ulatency.tree_loaded(subsys) then
+                task.data.do_not_report_move_errors[subsys] = true
+              end
+            end
+          end
+          if #cgroups > 1 then
+            props[#props+1] = "cgroups / in every subsys except "..
+                              table.concat(cgroups,",")
+          else
+            props[#props+1] = "cgroups: / in every subsys"
+          end
+          -- if process is in root cgroup of every subsystem then block it
+          -- from scheduler, this is expected as some kernel processes are
+          -- not movable, but I could not find the pattern, therefore they are
+          -- detected here
+          if in_root_cgroups then
+            proc:set_block_scheduler(1)
+            info[#info+1] = "This is probably kernel unmovable task"..
+                  " and will not be scheduled again."
+            log_level = ulatency.LOG_LEVEL_SCHED
+          -- otherwise just report
+          else
+            info[#info+1] = "This may be kernel unmovable task, but has"..
+                  " already been moved from root cgroup in some subsystem!"
+            info[#info+1] = "Therefore it won't be blocked from scheduling"..
+                  " but this error won't be printed again."
+            --proc:set_block_scheduler(0)
+          end
+        end -- proc
+
+      -- error while moving rt task in cpu subsystem
+      -- on kernel without CONFIG_RT_GROUP_SCHED
+      elseif cgr.tree == "cpu" and err_code == 22 and
+         posix.access(cgr:path("cpu.rt_runtime_us")) ~= 0
+      then
+        if task and ( task.sched == ulatency.SCHED_RR or
+                      task.sched == ulatency.SCHED_FIFO )
+        then
+          -- report only once
+          if reported_move_task_errors['cpu.rt_task'] then
+            return
+          end
+          reported_move_task_errors['cpu.rt_task'] = true
+          props[#props+1] = "RT sched"
+          info[#info+1] =
+                "\nYour kernel is probably compiled without"..
+                " CONFIG_RT_GROUP_SCHED; consider recompiling; no future"..
+                " error of this type will be logged."
+        end
+
+      -- cpuset: include cpuset.cpus and cpuset.mems value to the log
+      elseif cgr.tree == "cpuset" then
+        local files = {}
+        for _,file in ipairs{"cpuset.cpus", "cpuset.mems"} do
+          local fh = io.open(cgr:path(file))
+          if fh then
+            files[file] = fh:read("*a"):rtrim()
+            props[#props+1] = file.."="..files[file]
+            fh:close()
+          end
+        end
+      end
+
+      -- print error
+      if proc then
+        ulatency.log(log_level, string.format(
+              "Process (PID:%s, ppid:%s, euid:%s, egid:%s, cmd:%s, "..
+              "cmdfile:%s, exe:%s, cgr:%s%s) couldn't be moved to %s (%d:%s)%s",
+              tostring(value), proc.ppid or "?",
+              proc.euid or "?", proc.egid or "?",
+              proc.cmd or "?", proc.cmdfile or "?", proc.exe or '?',
+              proc:get_cgroup(subsys) or "?",
+              #props > 0 and ", "..table.concat(props,", ") or "",
+              tostring(cgr), err_code, err,
+              #info > 0 and " "..table.concat(info," ") or "" ))
+      else
+        ulatency.log(log_level, string.format(
+              "Task (TID:%s, ppid:%s, euid:%s, egid:%s, cmd:%s%s)"..
+              " couldn't be moved to %s (%d:%s)%s",
+              tostring(value), task.ppid or "?",
+              task.euid or "?", task.egid or "?",
+              task and task.cmd or "?",
+              #props > 0 and ", "..table.concat(props,", ") or "",
+              tostring(cgr), err_code, err,
+              #info > 0 and " "..table.concat(info," ") or "" ))
+      end
+      return
+    end -- cgr and file == "tasks"
+
+    ----------------------------------------------------------------------------
+    -- End handling error: Task could not be added to a cgroup
+    ----------------------------------------------------------------------------
+
+    -- other error
+    ulatency.log_warning(string.format(
+          "Can't write string '%s' into %s: (%d) %s",
+          tostring(value), filepath, err_code,err ))
+  end -- sysfs_write_error
+
+end -- do
 
 --! @brief Write string to a file under SYSFS
 function sysfs_write(path, value, quiet)
